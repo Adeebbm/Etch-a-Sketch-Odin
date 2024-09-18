3ed6911623995db9027bc29352546ed041fbf497
adeeb boumoghlbey <adeebboumoghlbey@users.noreply.github.com>
2024-09-18 11:43:06 +0300

    Add js to brush and eraser button and add addFirstPixel()
    add event listeners to each color button for the brush
    and the background color.
    
    addFirstPixel function adds a sigle div into the sketch
    container when the page loads so that if any of the
    background color buttons is pressed the user can see
    a responce (previously the user had to first manually enter a scale
    to then be able to see the background color)

--- javascript.js
+++ javascript.js
@@ -4,6 +4,13 @@ function clearPixels() {
     pixels.forEach(pixel => container.removeChild(pixel));
 }
 
+function addFirstPixel(){
+    const pixel = document.createElement('div');
+    pixel.style.height = `${container_height}px`;
+    pixel.style.width = `${container_width}px`;
+    container.appendChild(pixel);
+}
+
 let scale_submit = document.querySelector(".scale-submit-button");
 let input = document.querySelector("input");
 let container = document.querySelector(".sketch");
@@ -16,6 +23,12 @@ let brush_color = 'black';
 let background_color_buttons = document.querySelectorAll(".background-colors .color-button");
 let brush_color_buttons = document.querySelectorAll(".brush-colors .color-button");
 
+let brush = false; //shows if brush button has been pressed or not
+let eraser = false; // shows if eraser button has been pressed or not
+let brush_button = document.querySelector(".brush-button-container");
+let eraser_button = document.querySelector(".eraser-button-container");
+
+addFirstPixel(); // Initially the sketch container is empty
 
 scale_submit.addEventListener('click', ()=>{ // Change scale according to input
     let inp = parseInt(input.value);
@@ -53,8 +66,22 @@ background_color_buttons.forEach((button)=>{
     });
 });
 
-brush_color_buttons.forEach((button),()=>{
+brush_color_buttons.forEach((button)=>{
     button.addEventListener('click',()=>{
         brush_color = button.style.backgroundColor;
     });
+});
+
+brush_button.addEventListener('click',()=>{
+    brush = true;
+    eraser = false;
+    brush_button.style.backgroundColor = "yellow";
+    eraser_button.style.backgroundColor = "white";
+});
+
+eraser_button.addEventListener('click',()=>{
+    eraser = true;
+    brush = false;
+    eraser_button.style.backgroundColor = "yellow";
+    brush_button.style.backgroundColor = "white";
 });
